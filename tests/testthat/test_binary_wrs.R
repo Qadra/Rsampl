78b3008f52c91863af8cfb0a1c04553c13c45de0
Kasper Eenberg <kse@mahavira.dk>
2016-10-25 15:34:31 +0200

    Add more cases to test sampling shouldn't fail.

--- tests/testthat/test_binary_wrs.R
+++ tests/testthat/test_binary_wrs.R
@@ -20,18 +20,21 @@ test_that("indices and weights must have the same length", {
 			  expect_error(Rsampl::sample(S, W, k=k))
 })
 
-test_that("sampling should work", {
-			  S <- 1:100
-			  W <- runif(100)
-			  W <- W/sum(W)
-			  k <- 10
+for (n in c(32, 45, 50, 64, 95, 100)) {
+	test_that(paste("sampling should work with n =", n, "and k = 10"), {
+				  S <- 1:n
+				  W <- seq(1/n, n)
+				  k <- 10
 
-			  res <- Rsampl::sample(S, W, k=k)
+				  # Set seed to fixed value to make randomness repeatable
+				  set.seed(142)
+				  res <- Rsampl::sample(S, W, k=k)
 
-			  expect_equal(length(res), k)
+				  expect_equal(length(res), k)
 
-			  # Simple test
-			  for (i in 1:k) {
-				  expect_true(res[i] >= 0 && res[i] <= 100)
-			  }
-})
+				  # Simple test
+				  for (i in 1:k) {
+					  expect_true(res[i] >= 0 && res[i] <= n)
+				  }
+	})
+}
