4ad434497ddbcd77d4529698144d5852a0ae8561
Kasper Eenberg <kse@mahavira.dk>
2016-10-25 14:37:24 +0200

    Add codecov support, docs for 'sample'

--- R/sample.R
+++ R/sample.R
@@ -1,3 +1,11 @@
+#' Perform Weighted Random Sampling
+#' 
+#' @param A A vector/list of indices to sample
+#' @param W The weight of each corresponding index in A
+#' @param k The number of indices to sample
+#' @return \code{k} indices sampled according to weights
+#' @examples
+#' sample(1:100, rep(1/100, 100), k = 10)
 #' @export
 sample <- function(A, W, k = 1) {
 	if (length(A) != length(W)) {
